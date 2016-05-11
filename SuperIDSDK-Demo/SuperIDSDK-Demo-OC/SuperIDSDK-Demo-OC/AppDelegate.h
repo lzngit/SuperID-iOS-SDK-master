2724c11c216b7e79399eb53a090e2f8e665dfe03
lzngit <lzngithub@163.com>
2016-05-11 18:41:55 +0800

    First Commit.
    Facilitate developer.

---
+++ SuperIDSDK-Demo/SuperIDSDK-Demo-OC/SuperIDSDK-Demo-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperIDSDK-Demo-OC
+//
+//  Created by lzn on 16/5/11.
+//  Copyright © 2016年 SuperID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperIDSDK-Demo/SuperIDSDK-Demo-OC/SuperIDSDK-Demo-OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperIDSDK-Demo-OC
+//
+//  Created by lzn on 16/5/11.
+//  Copyright © 2016年 SuperID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
