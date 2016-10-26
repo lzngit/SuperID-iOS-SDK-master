8beaee1f12a9c71b98d949b55687ac5796141e7b
lzngit <lzngithub@163.com>
2016-10-26 18:12:46 +0800

    update SuperIDSDK
    update folder SuperIDSDK

--- SuperIDSDK/SuperID.h
+++ SuperIDSDK/SuperID.h
@@ -8,48 +8,84 @@
 
 #import <Foundation/Foundation.h>
 #import "SuperIDDelegate.h"
+#import <UIKit/UIKit.h>
+#import "SIDImageFilterCommon.h"
+#import "SIDAppUserInfoModel.h"
 
+
+#define SIDCURRENTUSERINFODATA @"SIDCurrentUserInfoData"
 
 
 @interface SuperID : NSObject
 
 
 
-/** SDK 委托对象 */
-@property(weak,nonatomic) id<SuperIDDelegate> delegate;
-
-
-
 /**
- *  SuperID 单例生成类方法
+ *  获取 SuperID 单例对象
  *
- *  @return SuperID单例对象
+ *  @return SuperID 单例对象
  */
 + (SuperID *)sharedInstance;
 
 
+/**
+ *  SuperID 设置单例委托对象
+ *
+ */
++ (void)setupSuperIDDelegate:(id<SuperIDDelegate>)delegate;
+
+
+
 
 /**
- *  SuperID的实例方法，向SDK注册第三方应用。
+ *  设置SDK主题颜色
+ *
+ *  @param color UIColor对象
+ */
++ (void)setThemeColor:(UIColor *)color;
+
+
+
+/**
+ *  移除SDK主题颜色，恢复SDK默认主题颜色
+ *
+ */
++ (void)removeThemeColor;
+
+/**
+ *  删除默认存本地的手机号码
+ *
+ */
++ (void)deleteSIDDefaultMobilePhoneNumber;
+
+
+
+/**
+ *  SuperID的注册方法，向SDK注册第三方应用。
  *  需要在每次启动第三方应用程序时调用。
  *  @param appID     开发者创建应用的应用ID
  *  @param appSecret 开发者创建应用的应用Secret
  *
- *  @return SuperID单例对象
  */
-- (void)registerAppWithAppID:(NSString *)appID withAppSecret:(NSString *)appSecret;
++ (void)registerAppWithAppID:(NSString *)appID withAppSecret:(NSString *)appSecret;
+
+
 
 
 /**
  *  SuperID的类方法，用于获取当前SDK的版本信息
  */
 + (NSDictionary *)getSDKVersion;
+
+
+
 /**
  *  SuperID的类方法，用于设置SDK在开发者应用中的语言模式，分自动模式、英文模式、简体中文模式、繁体中文模式。
  *
  *  @param type 语言类型详见枚举参数SIDLanguageMode解析
  */
 + (void)setLanguageMode:(SIDLanguageMode)type;
+
 
 
 
@@ -61,14 +97,19 @@
 + (void)setDebugMode:(BOOL)flag;
 
 
+
 /**
- *  SuperID的实例方法，获取一登登录VC。开发者使用present/show方法调用Super的登录界面
+    SuperID的实例方法，获取一登登录VC。开发者使用present/show方法调用Super的登录界面
  *  用户登录操作结果可通过实现SuperID的协议方法获取。
- *  @param error 主调用方传入NSError的指针的指针，来获取错误信息。开发者可根据错误描述判断错误情况。
+
+ *   @param userInfoModel 当前用户的应用账号信息，传入后可加快授权绑定过程。具体用法参考开发文档。如无，可置为nil。
+ *   @param error         主调用方传入NSError的指针的指针，来获取错误信息。开发者可根据错误描述判断错误情况。
  *
- *  @return SuperID登录VC的实例
+ *   @return SuperID登录VC的实例
  */
-- (instancetype)obtainLoginViewControllerWithError:(NSError **)error;
++ (UINavigationController *)obtainLoginViewControllerWithAppUserInfoModel:(SIDAppUserInfoModel *)userInfoModel error:(NSError **)error;
+
+
 
 
 /**
@@ -78,26 +119,28 @@
  *
  *  @return 一登人脸验证VC的实例
  */
-- (instancetype)obtainFaceVerifyViewControllerWithRetryCount:(NSNumber *)count error:(NSError **)error;
++ (UINavigationController *)obtainFaceVerifyViewControllerWithRetryCount:(NSNumber *)count error:(NSError **)error;
+
+
 
 
 /**
- *  SuperID的实例方法，获取一登登录VC。开发者使用present/show方法调用Super的登录界面
+ *  SuperID的实例方法，获取乐视专用一登登录VC。开发者使用present/show方法调用Super的登录界面
  *  用户登录操作结果可通过实现SuperID的协议方法获取。
- *  @param phoneNumber 当前用户的手机号码，当开发者在用户登录应用时已知用户手机号码情况下调用该接口。
  *  @param error       主调用方传入NSError的指针的指针，来获取错误信息。开发者可根据错误描述判断错误情况。
- *  @param userInfo    当前用户的应用账号信息，传入后可加快授权绑定过程。具体用法参考开发文档。如无，可置为nil。
  *  @return SuperID登录VC的实例
  */
-- (instancetype)obtainLoginViewControllerWithPhoneNumber:(NSString *)phoneNumber appUserInfo:(NSDictionary *)userInfo error:(NSError **)error;
++ (UINavigationController *)obtainLELoginViewControllerWithError:(NSError **)error;
+
 
 
 /**
- *  SuperID的实例方法，查询当前用户OpenID的授权状态
+ *  SuperID的实例方法，查询当前用户Uid的授权状态
  *  查询结果可通过实现SuperID的协议方法获取。
- *  @param openId 开发者应用中用户的openId
+ *
+ *   @param openId 开发者应用中用户的Uid
  */
-- (void)queryCurrentUserAuthorizationStateWithOpenId:(NSString *)openId;
++ (void)queryCurrentUserAuthorizationStateWithOpenId:(NSString *)openId;
 
 
 
@@ -109,19 +152,54 @@
  *
  *  @return 一登人脸信息VC的实例
  */
-- (instancetype)obtainFaceFeatureViewControllerWithError:(NSError **)error;
++ (UINavigationController *)obtainFaceFeatureViewControllerWithError:(NSError **)error;
+
+
 
 
 /**
  *  用户取消当前应用账号与一登账号的授权接口
  */
-- (void)userCancelAuthorization;
++ (void)userCancelAuthorization;
+
 
 
 /**
  *  开发者应用退出登录时，调用该接口，用户清除SuperID账号的Token信息。
  */
-- (void)appUserLogoutCurrentAccount;
++ (void)appUserLogoutCurrentAccount;
+
+
+
+
+/**
+ *  基于三方有效sso Token 创建一登账号
+ *
+ *  @param ssoToken 三方tk
+ */
++ (void)SIDCreatUserWithLESSOToken:(NSString *)ssoToken;
+
+
+
+
+/**
+ *  获取未注册一登用户的待验证手机号信息
+ *
+ *  @return 手机号信息：包括地区号和手机号
+ */
++ (NSDictionary *)getPreSignUpUserPhoneInfo;
+
+
+
+
+/**
+ *  获取一登用户的用户画像
+ *
+ *  @return 字典：一登用户画像
+ */
++ (NSDictionary *)getSIDUserInfoData;
+
+
 
 
 
@@ -130,21 +208,41 @@
  *
  *  @param userInfo 当前用户的应用账户信息字典。
  */
-- (void)updateAppUserInfo:(NSDictionary *)userInfo;
++ (void)updateAppUserInfo:(NSDictionary *)userInfo;
+
+
 
 
 /**
  *  判断当前用户与一登账号的授权情况。仅用于SDK的Demo
- *  开发者应调用接口：- (void)queryCurrentUserAuthorizationStateWithUid:(NSString *)uid;
+ *  开发者应调用接口：+ (void)queryCurrentUserAuthorizationStateWithUid:(NSString *)uid;
  *  @return 授权返回YES, 未授权返回NO.
  */
-- (BOOL)isAppAuth;
++ (BOOL)isAppAuth;
+
+
+
 
 
 /**
  *  当应用启动处于无网络状态下启动时，重新连接网络时可调用改接口重新初始化SuperID的SDK。
  */
-- (void)retryInitialSuperIDSDK;
++ (void)retryInitialSuperIDSDK;
+
+
+
+
+/**
+ *  当开发者应用兼容多方向适配时，如果开发者当前 VC 继承了
+ *  - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+ *  该委托方法，需要在该方法中添加如下接口调用，已将屏幕旋转事件传递给 SuperID 的 UI 界面作为界面变换判断
+ *
+ *  @param size                 委托方法回调的size
+ *  @param coordinator          委托方法回到的coodinator
+ *  @param sourceViewController 当前 VC 指针，一直传入 self
+ */
++ (void)SIDViewControllerWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator fromSourceViewController:(UIViewController *)sourceViewController;
+
 
 
 
@@ -155,37 +253,58 @@
  *  @param attributes 该事件的属性字典：如歌曲名称、歌手信息等。
  *  @param actions    该事件的用户操作字典：如：收藏、分享、删除等操作记录。
  */
-- (void)uploadUserBehaviourEventDataWithActionTag:(NSString *)aTag attributes:(NSDictionary *)attributes actions:(NSDictionary *)actions;
++ (void)uploadUserBehaviourEventDataWithActionTag:(NSString *)aTag attributes:(NSDictionary *)attributes actions:(NSDictionary *)actions;
 
 
-/**
- *  SuperID的实例方法，查询当前用户Uid的授权状态
- *  查询结果可通过实现SuperID的协议方法获取。
- *  @param uid 开发者应用中用户的Uid
- */
-- (void)queryCurrentUserAuthorizationStateWithUid:(NSString *)uid NS_DEPRECATED_IOS(2_0, 6_0, "Please use OpenID Account instead of uid");
+
 
 
 /**
- *  SuperID的实例方法，获取一登授权VC.开发者使用present/show方法调用Super的授权界面
- *  用户授权操作结果可通过实现SuperID的协议方法获取。
- *  @param uid         用于与用户SuperID账号授权绑定的用户Uid
- *  @param phoneNumber 当前用户的手机号码，传入后可加快授权绑定过程。如无，可置为nil。
- *  @param userInfo    当前用户的应用账号信息，传入后可加快授权绑定过程。具体用法参考开发文档。如无，可置为nil。
- *  @param error       主调用方传入NSError的指针的指针，来获取错误信息。开发者可根据错误描述判断错误情况。
+ *  获取当前用户的人脸头像
  *
- *  @return SuperID授权VC的实例
  */
++ (NSData *)getSIDCurrentUserAvatar;
 
-- (instancetype)obtainAuthViewControllerWithUid:(NSString *)uid phoneNumber:(NSString *)phoneNumber appUserInfo:(NSDictionary *)userInfo error:(NSError **)error NS_DEPRECATED_IOS(2_0, 6_0, "Please use OpenID Account instead of uid");
 
 
+
+
++ (void)updateSuperIDAccountInfoWith:(NSDictionary *)info avatar:(NSData *)avatar;
+
+
++ (NSString *)SIDHostName;
+
+
++ (void)clearCache;
+
+
++ (void)resetAppToken;
+
+
++ (void)setupLoginViewCameraPositon:(SIDPreferCameraDevicePosition) positon;
+
+
+
++ (void)queryCurrentUserAuthorizationStateWithUid:(NSString *)uid;
 
 /**
  *  当用户首次直接使用一登账号登录时，SuperID将返回关联的随机应用id。
  *  开发者获取当前用户的正式账号id后，调用该接口进行id更新。
  *  @param uid 当前用户的应用id。
  */
-- (void)updateAppUid:(NSString *)uid NS_DEPRECATED_IOS(2_0, 6_0, "Please use OpenID Account instead of uid");
++ (void)updateAppUid:(NSString *)uid;
+
+
+
++ (UINavigationController *)obtainAdvancedFaceFeatureViewControllerWithError:(NSError **)error;
+
+
+/**
+ *  必须在调用了初始化之后才执行，否则会被初始化的程序覆盖channelID
+ *
+ *  @param channel 频道ID
+ */
++ (void)setSIDChannel:(NSString *)channel;
+
 
 @end
